10fd8b7eb0a8f9b59527775f52288db2ded2ff68
martucappelloni@gmail.com <martucappelloni@gmail.com>
2021-04-09 16:19:52 -0300

    rutas

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
+var registerRouter = require('./routes/register');
+var loginRouter = require('./routes/login');
+var perfilRouter = require('./routes/perfil');
+var productosRouter = require('./routes/productos');
+var resultadosRouter = require('./routes/resultados');
+
+
 
 var app = express();
 
@@ -20,7 +27,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
+app.use('/perfil', perfilRouter);
+app.use('/productos', productosRouter);
+app.use('/resultados', resultadosRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
