7d3759ee86ee380abe3cce7f1e4583b5518f51df
martucappelloni@gmail.com <martucappelloni@gmail.com>
2021-04-15 21:35:46 -0300

    header

--- data/product.js
+++ data/product.js
@@ -1,28 +1,33 @@
 let product = [
     {
         id: 1,
-        modelo: 'Air Jordan',
-        imagen: '/images/products/jordans.jpg'
+        modelo: 'Air Jordan 1',
+        imagen: '/images/products/jordans.jpg',
+        descripcion: 'esta es la descripcion del producto 1',
     },
     {
         id: 2,
-        modelo: 'Air Jordan 1',
+        modelo: 'Air Jordan 2',
         imagen: '/images/products/jordans1.jpg',
+        descripcion: 'esta es la descripcion del producto 2',
     },
     {
         id: 3,
-        modelo: 'Air Jordan 2',
-        imagen: '/images/products/jordans2.jpg'
+        modelo: 'Air Jordan 3',
+        imagen: '/images/products/jordans2.jpg',
+        descripcion: 'esta es la descripcion del producto 3',
     },
     {
         id: 4,
-        modelo: 'Air Jordan 3',
-        imagen: '/images/products/jordans3.jpg'
+        modelo: 'Air Jordan 4',
+        imagen: '/images/products/jordans3.jpg',
+        descripcion: 'esta es la descripcion del producto 4',
     },
     {
         id: 5,
-        modelo: 'Air Jordan 4',
-        imagen: '/images/products/jordans4.jpg'
+        modelo: 'Air Jordan 5',
+        imagen: '/images/products/jordans4.jpg',
+        descripcion: 'esta es la descripcion del producto 5',
     }
 ]
 
