c74866bb2d37e909f6bd2ff6ec4acd960ee4d132
Lola Marinelli <lmarinelli@udesa.edu.ar>
2021-04-19 10:39:22 -0300

    Hicimos formularios

--- data/product.js
+++ data/product.js
@@ -4,30 +4,35 @@ let product = [
         modelo: 'Air Jordan 1',
         imagen: '/images/products/jordans.jpg',
         descripcion: 'esta es la descripcion del producto 1',
+        comentarios: 10
     },
     {
         id: 2,
         modelo: 'Air Jordan 2',
         imagen: '/images/products/jordans1.jpg',
         descripcion: 'esta es la descripcion del producto 2',
+        comentarios: 2
     },
     {
         id: 3,
         modelo: 'Air Jordan 3',
         imagen: '/images/products/jordans2.jpg',
         descripcion: 'esta es la descripcion del producto 3',
+        comentarios: 7
     },
     {
         id: 4,
         modelo: 'Air Jordan 4',
         imagen: '/images/products/jordans3.jpg',
         descripcion: 'esta es la descripcion del producto 4',
+        comentarios: 13
     },
     {
         id: 5,
         modelo: 'Air Jordan 5',
         imagen: '/images/products/jordans4.jpg',
         descripcion: 'esta es la descripcion del producto 5',
+        comentarios: 11
     }
 ]
 
