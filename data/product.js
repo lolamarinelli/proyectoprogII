216c5865215b436a710b6da8a080e5b72e7d71c6
martucappelloni@gmail.com <martucappelloni@gmail.com>
2021-04-22 12:27:43 -0300

    base de datos

--- data/product.js
+++ data/product.js
@@ -1,72 +1,72 @@
 let product = [
     {
         id: 1,
-        modelo: 'Air Jordan 1',
-        imagen: '/images/products/jordans.jpg',
-        descripcion: 'esta es la descripcion del producto 1',
+        modelo: 'JORDAN 1 HIGH SATIN SNAKE',
+        imagen: '/images/products/satinsnake.jpeg',
+        descripcion: 'Las zapatillas Jordan 1 HIGH SATIN SNAKE presentan una mezcla de satén, cuero y piel de serpiente sintética',
         comentarios: 10
     },
     {
         id: 2,
-        modelo: 'Air Jordan 2',
-        imagen: '/images/products/jordans1.jpg',
-        descripcion: 'esta es la descripcion del producto 2',
+        modelo: 'JORDAN 1 MID MAGENTA',
+        imagen: '/images/products/magenta.jpeg',
+        descripcion: 'Las Jordan 1 MID MAGENTA son el primer zapato exclusivo de Michael Jordan equipado en talla de mujer de color magenta, negro y blanco',
         comentarios: 2
     },
     {
         id: 3,
-        modelo: 'Air Jordan 3',
-        imagen: '/images/products/jordans2.jpg',
-        descripcion: 'esta es la descripcion del producto 3',
+        modelo: 'SB DUNK SPARTAN GREEN',
+        imagen: '/images/products/spartangreen.jpeg',
+        descripcion: 'Las SB DUNK SPARTAN GREEN son las zapatillas verdes que no te quitarás en todo el otoño',
         comentarios: 7
     },
     {
         id: 4,
-        modelo: 'Air Jordan 4',
-        imagen: '/images/products/jordans3.jpg',
-        descripcion: 'esta es la descripcion del producto 4',
+        modelo: 'JORDAN 1 CRIMSON TINT',
+        imagen: '/images/products/crimsontint.jpeg',
+        descripcion: 'Las zapatillas JORDAN 1 CRIMSON TINT añaden un tono rosa coral espectacular. ¿Qué estas esperando? Compralas ya!',
         comentarios: 13
     },
     {
         id: 5,
-        modelo: 'Air Jordan 5',
-        imagen: '/images/products/jordans4.jpg',
-        descripcion: 'esta es la descripcion del producto 5',
+        modelo: 'JORDAN 1 MID MULTICOLOR',
+        imagen: '/images/products/multicolor.jpeg',
+        descripcion: 'Las zapatillas JORDAN 1 MID MULTICOLOR son solamente para aquellos que les gusta divertirse. Ya su nombre lo delata, tienen todos los colores!',
         comentarios: 11
     },
     {
         id: 6,
-        modelo: 'Air Jordan 6',
-        imagen: '/images/products/jordans4.jpg',
-        descripcion: 'esta es la descripcion del producto 6',
-        comentarios: 25
+        modelo: 'JORDAN 1 OBSIDIAN',
+        imagen: '/images/products/obsidian.jpeg',
+        descripcion: 'Las zapatillas de color Obsidian, coinciden con los colores del equipo de la Universidad de Carolina del Norte, en donde Michael Jordan estudió y jugó antes de dar el salto a la NBA',
+        comentarios: 7
     },
     {
         id: 7,
-        modelo: 'Air Jordan 7',
-        imagen: '/images/products/jordans.jpg',
-        descripcion: 'esta es la descripcion del producto 7',
+        modelo: 'JORDAN 1 PINE GREEN',
+        imagen: '/images/products/pinegreen.jpeg',
+        descripcion: 'Las zapatillas JORDAN 1 PINE GREEN son increibles para aquellos que les encanta el color verde, blanco y negro',
         comentarios: 10
     },
     {
         id: 8,
-        modelo: 'Air Jordan 8',
-        imagen: '/images/products/jordans1.jpg',
-        descripcion: 'esta es la descripcion del producto 8',
+        modelo: 'JORDAN 1 ROOKIE OF THE YEAR',
+        imagen: '/images/products/rookieoftheyear.jpeg',
+        descripcion: 'Las zapatillas JORDAN 1 ROOKIE OF THE YEAR de color piel, son de un gran confort y tacto premium. Además, cuenta con una puntera perforada para una mayor durabilidad',
         comentarios: 2
     },
     {
         id: 9,
-        modelo: 'Air Jordan 9',
-        imagen: '/images/products/jordans2.jpg',
-        descripcion: 'esta es la descripcion del producto 9',
-        comentarios: 7
+        modelo: 'JORDAN 1 YELLOW OCHRE',
+        imagen: '/images/products/yellowochre.jpeg',
+        descripcion: 'Las zapatillas JORDAN 1 YELLOW OCHRE son parte del paquete Best Hand in the Game, vienen en una combinación de colores blanco cumbre, ocre amarillo y negro',
+        comentarios: 25
     },
     {
         id: 10,
-        modelo: 'Air Jordan 10',
-        imagen: '/images/products/jordans3.jpg',
-        descripcion: 'esta es la descripcion del producto 10',
+        modelo: 'Air Jordan 1 RETRO HIGH Off-White CHICAGO',
+        imagen: '/images/products/retrohigh.jpeg',
+        descripcion: 'Las zapatillas AIR JORDAN 1 RETRO HIGH Off-White CHICAGO son una de las zapatillas más vendidas de Nike en relación con la practica del baloncesto, desde sus orígenes siempre ha destacado por su gran versatilidad sobre la pista de basket',
         comentarios: 13
     },
 ]
