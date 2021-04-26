a656c8fbe04e3413881e638883fdc58d20e5e449
martucappelloni@gmail.com <martucappelloni@gmail.com>
2021-04-26 02:15:45 -0300

    controller

--- Controllers/indexController.js
+++ Controllers/indexController.js
@@ -5,16 +5,6 @@ let controller = {
     index: function (req, res){
         res.render('index', {'product': product, profile})
     },
-    // id: function(req, res){
-    //     let ids = req.params.id
-    //     let resultados = []
-    //     for (let i = 0; i < product.length; i++) {
-    //         if (product[i].id == ids){
-    //             resultados.push(product[i])  
-    //         }            
-    //     }
-    //     res.render("product", {'product': resultados, profile, product})
-    // },
 }
 
 module.exports = controller;
