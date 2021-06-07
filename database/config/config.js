1f9dc6886603698aa4212c24d59dda459ab65ec6
Mora Baylac <morabaylac@MacBook-Pro-de-Mora.local>
2021-06-07 12:06:13 -0300

    database

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "root",
-    "database": "proyectoprog2_coolkicks",
+    "database": "proyectoprog2",
     "host": "127.0.0.1",
     "port": "8889",
     "dialect": "mysql"
