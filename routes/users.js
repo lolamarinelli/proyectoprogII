ef308cbf59022bac9c63bb0c06efa179e3bae8c5
lolamarinelli <lmarinelli@udesa.edu.ar>
2021-06-24 12:16:05 -0300

    avatar

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ let path = require("path")
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/images/avatar') // en donde vamos a guardar nuestras imagenes
+      cb(null, 'public/images/users') // en donde vamos a guardar nuestras imagenes
     },
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
